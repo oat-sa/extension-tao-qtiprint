f1b6909993d0817d990855f394d04e6c67d37e6d
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-04-25 18:12:32 +0300

    removed referring to a variable BASE_WWW

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -33,7 +33,7 @@ class Updater extends \common_ext_ExtensionUpdater {
      */
     public function update($initialVersion) {
 
-        $this->skip('0.1.0','0.2.1');
+        $this->skip('0.1.0','1.0.0');
 
     }
 }
