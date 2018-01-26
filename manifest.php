b2e7c5cc4fc7c16887ba90dcdbf117959241e774
Ilya Yarkavets <ilya.yarkavets@1pt.com>
2018-01-26 14:13:33 +0300

    License change & version bump;

--- manifest.php
+++ manifest.php
@@ -23,8 +23,8 @@ return array(
     'name' => 'taoQtiPrint',
 	'label' => 'qti',
 	'description' => 'Provides printable rendering for QTI Items',
-    'license' => 'GPL-2.0',
-    'version' => '1.7.0',
+    'license' => 'GPL-2.0-only',
+    'version' => '1.7.1',
 	'author' => 'Open Assessment Technologies SA',
 	'requires' => array(
 	    'tao' => '>=9.0.0',
